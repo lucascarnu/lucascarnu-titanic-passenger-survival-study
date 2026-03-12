235b2a9f3401ff842baef6f76ea77f47d5495204
Lucas Carnuccio <lucascarnu@gmail.com>
2026-03-12 20:41:19 -0300

    Add data visualizations using matplotlib

--- src/analysis.py
+++ src/analysis.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 
 # Load the Titanic dataset
 data = pd.read_csv("dataset/titanic.csv")
@@ -42,3 +43,32 @@ print(data.groupby("sex")["survived"].mean())
 # Survival rate by passenger class
 print("\nSurvival by class:")
 print(data.groupby("pclass")["survived"].mean())
+
+# Survival rate by gender (visualization)
+print("\nPlotting survival rate by gender...")
+
+data.groupby("sex")["survived"].mean().plot(kind="bar")
+plt.title("Survival Rate by Gender")
+plt.xlabel("Gender")
+plt.ylabel("Survival Rate")
+plt.show()
+
+
+# Survival rate by passenger class (visualization)
+print("\nPlotting survival rate by passenger class...")
+
+data.groupby("pclass")["survived"].mean().plot(kind="bar")
+plt.title("Survival Rate by Passenger Class")
+plt.xlabel("Passenger Class")
+plt.ylabel("Survival Rate")
+plt.show()
+
+
+# Age distribution of passengers
+print("\nPlotting age distribution...")
+
+data["age"].hist(bins=20)
+plt.title("Age Distribution of Passengers")
+plt.xlabel("Age")
+plt.ylabel("Number of Passengers")
+plt.show()
